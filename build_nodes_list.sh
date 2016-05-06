81e8bd4811bf08e763b0cd02f37afa97aca7bc50
dre <dre@rpb.ru>
2016-05-06 14:11:20 +0300

    Balancing changed from http to ajp and bybusiness method

--- build_nodes_list.sh
+++ build_nodes_list.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 IPS=`ansible-playbook -v nodes_ip.yml |grep %% |sed 's/^.*%%\([0-9\.]\+\)%%.*$/\1/g'`
-NODES="files/nodes.list"
+HNODES="files/nodes.http_list"
+ANODES="files/nodes.ajp_list"
 rm -f $NODES
 for IP in $IPS; do
   if [[ $IP =~ ([0-9]+)$ ]];then
     NUM=${BASH_REMATCH[1]};
-    NODE="BalancerMember http://$IP:8080 route=$NUM"
-    echo $NODE
-    echo $NODE >> $NODES 
+    HNODE="BalancerMember http://$IP:8080 route=$NUM"
+    ANODE="BalancerMember ajp://$IP:8009  loadfactor=1 route=$NUM"
+    echo $HNODE
+    echo $ANODE
+    echo $HNODE >> $HNODES 
+    echo $ANODE >> $ANODES 
   fi
 done
