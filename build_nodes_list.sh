677de0e341b706f3228bdfc00f0785ba8548792f
dre <dre@rpb.ru>
2016-05-11 17:36:51 +0300

    Add autoupdated nodes list to script section in index.html

--- build_nodes_list.sh
+++ build_nodes_list.sh
@@ -2,7 +2,10 @@
 IPS=`ansible-playbook -v nodes_ip.yml |grep %% |sed 's/^.*%%\([0-9\.]\+\)%%.*$/\1/g'`
 HNODES="files/nodes.http_list"
 ANODES="files/nodes.ajp_list"
+HTML=html/test.html
+JHT="//JOLOKIA_HOSTS_TEMPLATES\n//###"
 rm -f $NODES
+IPSL=0
 for IP in $IPS; do
   if [[ $IP =~ ([0-9]+)$ ]];then
     NUM=${BASH_REMATCH[1]};
@@ -12,5 +15,11 @@ for IP in $IPS; do
     echo $ANODE
     echo $HNODE >> $HNODES 
     echo $ANODE >> $ANODES 
+    IPSL=$((IPSL+1))
   fi
 done
+for NUM in $(seq 1 $IPSL);do
+    JHT="$JHT\nj4p[$NUM] = new Jolokia(\"http://localhost:808$NUM/jolokia\");"
+done
+JHT="$JHT\n//###"
+sed -i '/\/\/###/,/\/\/###/d' $HTML && sed -i 's@//JOLOKIA_HOSTS_TEMPLATES@'"$JHT"'@' $HTML
