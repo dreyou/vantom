d77344ad9f1f05aaab199ebe6383407875d6d42e
dre <dre@rpb.ru>
2016-05-11 16:33:59 +0300

    Add bootstrap css

--- prep.sh
+++ prep.sh
@@ -5,6 +5,10 @@ HTML=./html
 if [[ ! -d $DIST ]];then
   mkdir -p $DIST
 fi
+if [[ ! -d $HTML ]];then
+  mkdir -p $HTML/js
+  mkdir -p $HTML/css
+fi
 wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M4/bin/apache-tomcat-9.0.0.M4.tar.gz -O $DIST/apache-tomcat-9.0.0.M4.tar.gz
 wget https://tomcat.apache.org/tomcat-9.0-doc/appdev/sample/sample.war -O $DIST/sample.war
 wget http://wiki.dreyou.org/pub/misc/spring-boot/sampledeploy-0.0.1-SNAPSHOT.war -O $DIST/sampledeploy.war
@@ -13,3 +17,4 @@ wget https://github.com/rhuss/jolokia/releases/download/v1.3.3/jolokia-min.js -O
 wget https://github.com/rhuss/jolokia/releases/download/v1.3.3/jolokia-simple-min.js -O $HTML/js/jolokia-simple-min.js
 wget https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js -O $HTML/js/json2.js
 wget https://code.jquery.com/jquery-1.12.3.min.js -O $HTML/js/jquery.min.js
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css -O $HTML/css/bootstrap-theme.min.css
